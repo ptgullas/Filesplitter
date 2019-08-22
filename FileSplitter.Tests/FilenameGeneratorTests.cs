f7b5d979c2bde9fab3a4cb771caa51627b2b09bb
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-08-22 09:09:51 -0400

    Added FilenameGeneratorTests

---
+++ FileSplitter.Tests/FilenameGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSplitter.Tests {
+    [TestClass]
+    public class FilenameGeneratorTests {
+        [TestMethod]
+        public void GenerateFilename_StartingFilename_GeneratesCorrectly() {
+            // Arrange
+            string indexFile = "c:\\temp\\IA_2019_index.txt";
+            int totalLines = 400;
+            int linesPerFile = 50;
+            string expected = "IA_2019_index_1_001-050.txt";
+
+            FilenameGenerator fg = new FilenameGenerator(indexFile, totalLines, linesPerFile);
+
+            // Act
+            string result = fg.GenerateFilename();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
