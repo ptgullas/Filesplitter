002b8be77b4e5d05ac439996e45d342172f8145a
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-08-24 10:08:07 -0400

    Added ColorHelpers

---
+++ FileSplitter/ColorHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSplitter {
+    public static class ColorHelpers {
+
+        public static void WriteColor(string str, ConsoleColor fontColor = ConsoleColor.White, ConsoleColor backColor = ConsoleColor.Black) {
+            Console.ForegroundColor = fontColor;
+            Console.BackgroundColor = backColor;
+            Console.Write(str);
+            Console.ResetColor();
+        }
+
+        public static void WriteLineColor(string str, ConsoleColor fontColor = ConsoleColor.White, ConsoleColor backColor = ConsoleColor.Black) {
+            Console.ForegroundColor = fontColor;
+
+            Console.BackgroundColor = backColor;
+            Console.WriteLine(str);
+            Console.ResetColor();
+        }
+    }
+}
