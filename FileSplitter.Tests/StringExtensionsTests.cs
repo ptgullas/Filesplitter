10fa0a6c48ede7e32125ad3c790cc5c7495df978
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-08-25 13:14:38 -0400

    Added ContainsWildcard string extension & tests

---
+++ FileSplitter.Tests/StringExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSplitter.Tests {
+    [TestClass]
+    public class StringExtensionsTests {
+        [TestMethod]
+        public void ContainsWildcard_ContainsAsterisk_ReturnsTrue() {
+            // Arrange
+            string strToTest = "*.txt";
+            // Act
+            bool result = strToTest.ContainsWildcard();
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ContainsWildcard_ContainsQuestionMark_ReturnsTrue() {
+            // Arrange
+            string strToTest = "?index.txt";
+            // Act
+            bool result = strToTest.ContainsWildcard();
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ContainsWildcard_ContainsAsteriskAndQuestionMark_ReturnsTrue() {
+            // Arrange
+            string strToTest = "?index*.txt";
+            // Act
+            bool result = strToTest.ContainsWildcard();
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
--- FileSplitter/StringExtensions.cs
+++ FileSplitter/StringExtensions.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace FileSplitter {
     public static class StringExtensions {
-        public static bool IsNumeric(this string source) {
-            return int.TryParse(source, out int i);
+        public static bool IsNumeric(this string str) {
+            return int.TryParse(str, out int i);
         }
 
-        public static int ToInt(this string source) {
+        public static int ToInt(this string str) {
             int result = 0;
-            if (source.IsNumeric()) {
-                bool b = int.TryParse(source, out result);
+            if (str.IsNumeric()) {
+                bool b = int.TryParse(str, out result);
             }
             return result;
+        }
+
+        public static bool ContainsWildcard(this string str) {
+            if (str.Contains('*') || str.Contains('?')) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 }
